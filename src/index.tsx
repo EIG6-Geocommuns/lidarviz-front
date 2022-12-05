8f1fc2321fb1b7e7d512747a6d9f0435c0b6bc35
maxi_malain <maxime.allain.8@gmail.com>
2022-12-05 16:41:28 +0100

    feat: create MapViewer page and route

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MapExtentSelector from "./pages/MapExtentSelector/MapExtentSelector";
+import MapViewer from "./pages/MapViewer/MapViewer";
 
 export enum ROUTES {
   Home = "/",
   MapExtentSelector = "/definition-emprise",
+  MapViewer = "/visualisation",
 }
 
 const router = createBrowserRouter([
   { path: ROUTES.Home, element: <Home /> },
   { path: ROUTES.MapExtentSelector, element: <MapExtentSelector /> },
+  { path: ROUTES.MapViewer, element: <MapViewer /> },
 ]);
 
 const root = ReactDOM.createRoot(
---
+++ src/pages/MapViewer/MapViewer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MapViewer = () => {
+  return <div>MapViewer</div>;
+};
+
+export default MapViewer;
