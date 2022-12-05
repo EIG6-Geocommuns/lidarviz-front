8a6957e97f956e2b083d3a254624eed8ebf75ada
maxi_malain <maxime.allain.8@gmail.com>
2022-12-05 18:19:01 +0100

    feat: give static params to MapViewer

--- src/pages/MapExtentSelector/MapExtentSelector.tsx
+++ src/pages/MapExtentSelector/MapExtentSelector.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
+import { ROUTES } from "../..";
 
 const MapExtentSelector = () => {
-  return <div>MapExtentSelector</div>;
+  const navigate = useNavigate();
+  const mapParams = createSearchParams({
+    WIDTH: "256",
+    HEIGHT: "256",
+    SRS: "EPSG:3857",
+    BBOX: "-13452.916978191584,6057481.617543647,-12229.924525628765,6058704.60999621",
+  });
+
+  return (
+    <div>
+      <p>MapExtentSelector</p>
+      <button onClick={() => navigate(ROUTES.MapViewer + "?" + mapParams)}>
+        Visualiser
+      </button>
+    </div>
+  );
 };
 
 export default MapExtentSelector;
--- src/pages/MapViewer/MapViewer.tsx
+++ src/pages/MapViewer/MapViewer.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const MapViewer = () => {
-  return <div>MapViewer</div>;
+  const [searchParams, _setSearchParams] = useSearchParams();
+
+  return (
+    <div>
+      MapViewer, params :
+      <ul>
+        {Array.from(searchParams.entries()).map((e) => (
+          <li>{e[0] + " : " + e[1]}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MapViewer;
