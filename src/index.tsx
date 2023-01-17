1fb31af50ebb46d44a7e52d03ca97a4d243bf338
maxi_malain <maxime.allain.8@gmail.com>
2023-01-17 15:55:18 +0100

    core: install dsfr

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import Home from "./pages/Home/Home";
 import MapExtentSelector from "./pages/MapExtentSelector/MapExtentSelector";
 import MapViewer from "./pages/MapViewer/MapViewer";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+startReactDsfr({ defaultColorScheme: "system" });
 
 export enum ROUTES {
   Home = "/",
