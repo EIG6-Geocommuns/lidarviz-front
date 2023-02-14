4d344053e80b74a6cf091f361381839904070081
maxi_malain <maxime.allain.8@gmail.com>
2023-02-14 12:49:42 +0100

    feat: instantiate inondata home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "tss-react/dsfr";
 import { fr } from "@codegouvfr/react-dsfr";
 import Button from "@codegouvfr/react-dsfr/Button";
-import { useNavigate } from "react-router-dom";
 import { ROUTES } from "..";
 
 const useStyles = makeStyles()((theme) => ({
@@ -51,71 +50,131 @@ const useStyles = makeStyles()((theme) => ({
   iconButton: {
     marginLeft: fr.spacing("1w"),
   },
+  inondataTitleBackground: {
+    backgroundColor: "rgba(35, 63, 123, 0.67)", //TODO: set other color if darkMode
+    textAlign: "center",
+    padding: fr.spacing("2w"),
+    [fr.breakpoints.down("md")]: { width: "90%" },
+    [fr.breakpoints.up("md")]: { width: "70%" },
+    [fr.breakpoints.up("lg")]: { width: "50%" },
+  },
 }));
+
+type Project = "Predictia" | "Inondata";
+
+const PROJECT_COVER: { [key in Project]: any } = {
+  Inondata: require("../assets/img/lidar_hd_marseille.png"),
+  Predictia: require("../assets/img/carte_de_predictions.png"),
+};
+
+const PROJECT_TITLE: { [key in Project]: string } = {
+  Predictia: "Cartes de prédictions",
+  Inondata: "Inondata",
+};
+
+const PROJECT_DESCRIPTION: { [key in Project]: string } = {
+  Predictia: "Prototype de démonstration",
+  Inondata: "Prototype de démonstration",
+};
+
+const PROJECT_TEXT: { [key in Project]: JSX.Element[] } = {
+  Predictia: [
+    <p>
+      Ce prototype est un outil de travail pour aider à la conception du
+      Catalogue Géodata qui remplacera prochainement les Géoservices. Nous
+      testons grâce à plusieurs prototypes les accès et les usages liés aux
+      différentes bases de données de l’IGN.
+      <br />
+      <b>Ici, seul les [Cartes de prédictions] feront l’objet d’un test.</b>
+    </p>,
+    <p>
+      En tant que testeur, nous vous invitons à naviguer dans cette première
+      version et à nous faire parvenir vos retours sur l’interface et ses
+      fonctionnalités. Notez que le prototype ne reflète pas l’identité finale
+      du catalogue Geodata. Les styles ne sont pas déterminés et les
+      terminologies et fonctionnalités ne sont pas figées.
+    </p>,
+    <p>
+      En tant que testeur, nous vous invitons à naviguer dans cette première
+      version et à nous faire parvenir vos retours sur l’interface et ses
+      fonctionnalités. Notez que le prototype ne reflète pas l’identité finale
+      du catalogue Geodata. Les styles ne sont pas déterminés et les
+      terminologies et fonctionnalités ne sont pas figées.
+    </p>,
+    <p>
+      Vous pouvez partager ce prototype à votre entourage professionnelle mais
+      nous vous demandons de ne pas communiquer dessus publiquement.
+    </p>,
+  ],
+  Inondata: [
+    <p>
+      Inondata est un visualisateur 3D disponible en ligne qui offre la
+      possibilité d’importer puis de visualiser les hauteurs et vitesses d’eau
+      d’une inondation potentielle par rapport à la description d’un territoire
+      fournie par les données LiDAR HD.
+    </p>,
+    <p>
+      Ce prototype est un outil de travail à destination des unités de
+      prévention des risques des Direction Départementale des Territoires (DDT),
+      pour aider à la conception de l’outil. Nous testons grâce à plusieurs
+      prototypes les accès, les visuels et les usages liés.
+    </p>,
+    <p>
+      En tant que testeur, nous vous invitons à naviguer dans cette première
+      version et à nous faire parvenir vos retours sur l’interface et ses
+      fonctionnalités. Notez que le prototype ne reflète pas l’identité finale
+      du démonstrateur. Les styles ne sont pas déterminés et les terminologies
+      et fonctionnalités ne sont pas figées.
+    </p>,
+    <p>
+      Vous pouvez partager ce prototype à votre entourage professionnel mais
+      nous vous demandons de ne pas communiquer dessus publiquement.
+    </p>,
+  ],
+};
 
 export const Home = () => {
   const { classes, cx } = useStyles();
-  const navigate = useNavigate();
+  const project: Project = "Inondata";
+
+  const PROJECT_CTA: { [key in Project]: JSX.Element } = {
+    Predictia: (
+      <Button linkProps={{ to: ROUTES.MapExtentSelector }} priority="secondary">
+        Accéder au prototype
+        <span
+          className={cx("fr-icon-arrow-right-line", classes.iconButton)}
+          aria-hidden="true"
+        ></span>
+      </Button>
+    ),
+    Inondata: (
+      <a className={fr.cx("fr-btn")} href="http://116.203.195.247/">
+        Accéder au Prototype
+        <span
+          className={cx("fr-icon-arrow-right-line", classes.iconButton)}
+          aria-hidden="true"
+        ></span>
+      </a>
+    ),
+  };
 
   return (
     <div>
       <div className={classes.titleBlock}>
-        <img
-          className={classes.cover}
-          src={require("../assets/img/carte_de_predictions.png")}
-          alt=""
-        />
-        <div className={classes.titleTexts}>
-          <h1 className={classes.title}>Cartes de prédictions</h1>
-          <p className={classes.description}>Prototype de démonstration</p>
+        <img className={classes.cover} src={PROJECT_COVER[project]} alt="" />
+        <div
+          className={cx(classes.titleTexts, classes.inondataTitleBackground)}
+        >
+          <h1 className={classes.title}>{PROJECT_TITLE[project]}</h1>
+          <p className={classes.description}>{PROJECT_DESCRIPTION[project]}</p>
         </div>
       </div>
 
       <main className={classes.main}>
         <h2 className={classes.subtitle}>Bienvenue</h2>
-        <div className={classes.texts}>
-          <p>
-            Ce prototype est un outil de travail pour aider à la conception du
-            Catalogue Géodata qui remplacera prochainement les Géoservices. Nous
-            testons grâce à plusieurs prototypes les accès et les usages liés
-            aux différentes bases de données de l’IGN.
-            <br />
-            <b>
-              Ici, seul les [Cartes de prédictions] feront l’objet d’un test.
-            </b>
-          </p>
-          <p>
-            En tant que testeur, nous vous invitons à naviguer dans cette
-            première version et à nous faire parvenir vos retours sur
-            l’interface et ses fonctionnalités. Notez que le prototype ne
-            reflète pas l’identité finale du catalogue Geodata. Les styles ne
-            sont pas déterminés et les terminologies et fonctionnalités ne sont
-            pas figées.
-          </p>
-          <p>
-            En tant que testeur, nous vous invitons à naviguer dans cette
-            première version et à nous faire parvenir vos retours sur
-            l’interface et ses fonctionnalités. Notez que le prototype ne
-            reflète pas l’identité finale du catalogue Geodata. Les styles ne
-            sont pas déterminés et les terminologies et fonctionnalités ne sont
-            pas figées.
-          </p>
-          <p>
-            Vous pouvez partager ce prototype à votre entourage professionnelle
-            mais nous vous demandons de ne pas communiquer dessus publiquement.
-          </p>
-        </div>
+        <div className={classes.texts}>{PROJECT_TEXT[project]}</div>
 
-        <Button
-          onClick={() => navigate(ROUTES.MapExtentSelector)}
-          priority="secondary"
-        >
-          Accéder au prototype
-          <span
-            className={cx("fr-icon-arrow-right-line", classes.iconButton)}
-            aria-hidden="true"
-          ></span>
-        </Button>
+        {PROJECT_CTA[project]}
       </main>
     </div>
   );
