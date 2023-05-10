7de902efc1ec553e62bf5264bc65e1d919133ea5
maxi_malain <maxime.allain.8@gmail.com>
2023-05-10 20:22:27 +0200

    feat: add Select

--- src/components/TerritorySelection.tsx
+++ src/components/TerritorySelection.tsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+import { makeStyles } from "@codegouvfr/react-dsfr/tss";
+import { fr } from "@codegouvfr/react-dsfr";
+import { Select } from "@codegouvfr/react-dsfr/SelectNext";
+
+const useStyles = makeStyles()(() => ({
+  title: {
+    marginBottom: fr.spacing("4w"),
+  },
+  container: {
+    maxWidth: 600,
+  },
+}));
+
 export const TerritorySelection = () => {
-  return <h2>Visualisation d'un territoire</h2>;
+  const { classes } = useStyles();
+  const [value, setValue] = useState("");
+
+  const VALUES = [
+    { value: "ddt84", label: "DDT 84" },
+    { value: "ddt45", label: "DDT 45" },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <h4 className={classes.title}>Visualisation d'un territoire</h4>
+      <Select
+        label="Territoires disponibles"
+        nativeSelectProps={{
+          onChange: (event) => setValue(event.target.value),
+          value,
+        }}
+        options={VALUES}
+      />
+    </div>
+  );
 };
