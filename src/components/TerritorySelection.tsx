86788ea36a2cbe2752bd6fba887bcf8851903962
maxi_malain <maxime.allain.8@gmail.com>
2023-05-15 18:14:36 +0200

    add button

--- src/components/TerritorySelection.tsx
+++ src/components/TerritorySelection.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { makeStyles } from "@codegouvfr/react-dsfr/tss";
 import { fr } from "@codegouvfr/react-dsfr";
 import { Select } from "@codegouvfr/react-dsfr/SelectNext";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import { ROUTES } from "..";
 
 const useStyles = makeStyles()(() => ({
   title: {
@@ -12,14 +14,20 @@ const useStyles = makeStyles()(() => ({
   },
 }));
 
+const VALUES = [
+  { value: "ddtm13", label: "DDTM 13" },
+  { value: "ddtm64", label: "DDTM 64" },
+  { value: "ddt67", label: "DDT 67" },
+  { value: "ddtm83", label: "DDTM 83" },
+  { value: "ddt84", label: "DDT 84" },
+  { value: "dreal-reunion", label: "DEAL Réunion" },
+];
+
 export const TerritorySelection = () => {
   const { classes } = useStyles();
   const [value, setValue] = useState("");
 
-  const VALUES = [
-    { value: "ddt84", label: "DDT 84" },
-    { value: "ddt45", label: "DDT 45" },
-  ];
+  const viewerHref = useMemo(() => ROUTES.Viewer.replace(":territoryId", value), [value]);
 
   return (
     <div className={classes.container}>
@@ -32,6 +40,10 @@ export const TerritorySelection = () => {
         }}
         options={VALUES}
       />
+
+      <a href={viewerHref}>
+        <Button disabled={value === ""}>Lancer la Visualisation</Button>
+      </a>
     </div>
   );
 };
