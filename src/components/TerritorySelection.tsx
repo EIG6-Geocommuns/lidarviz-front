5c6968eb6ce35da03eb4bb5b337d5e7f23f65b32
maxi_malain <32595427+maximallain@users.noreply.github.com>
2023-06-14 11:04:39 +0200

    feat: add mailto to ask for data adding (#51)
    * feat: add mailto to ask for data adding
    
    * create modal

--- src/components/TerritorySelection.tsx
+++ src/components/TerritorySelection.tsx
@@ -3,6 +3,8 @@ import { makeStyles } from "tss-react/dsfr";
 import { fr } from "@codegouvfr/react-dsfr";
 import { Select } from "@codegouvfr/react-dsfr/SelectNext";
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { createModal } from "@codegouvfr/react-dsfr/Modal";
+
 import { ROUTES } from "..";
 import { AvailableTerritoryId } from "../utils/waterLayers";
 
@@ -10,7 +12,12 @@ const useStyles = makeStyles()(() => ({
   title: {
     marginBottom: fr.spacing("4w"),
   },
-  container: {
+  visualization: {
+    maxWidth: 600,
+    marginBottom: fr.spacing("3w"),
+  },
+  container: {},
+  askForAddingData: {
     maxWidth: 600,
   },
 }));
@@ -25,6 +32,11 @@ const VALUES: { value: AvailableTerritoryId; label: string }[] = [
   { value: "ddt84", label: "84 - Vaucluse" },
 ];
 
+const modal = createModal({
+  id: "aks-for-adding-data-modal",
+  isOpenedByDefault: false,
+});
+
 export const TerritorySelection = () => {
   const { classes } = useStyles();
   const [value, setValue] = useState<AvailableTerritoryId | undefined>(undefined);
@@ -35,19 +47,40 @@ export const TerritorySelection = () => {
 
   return (
     <div className={classes.container}>
-      <h4 className={classes.title}>Visualisation d'un territoire</h4>
-      <Select
-        label="Territoires disponibles"
-        nativeSelectProps={{
-          onChange: (event) => setValue(event.target.value),
-          value,
-        }}
-        options={VALUES}
-      />
+      <div className={classes.visualization}>
+        <h4 className={classes.title}>Visualisation d'un territoire</h4>
+        <Select
+          label="Territoires disponibles"
+          nativeSelectProps={{
+            onChange: (event) => setValue(event.target.value),
+            value,
+          }}
+          options={VALUES}
+        />
 
-      <a href={viewerHref}>
-        <Button disabled={value === undefined}>Lancer la Visualisation</Button>
-      </a>
+        <a href={viewerHref}>
+          <Button disabled={value === undefined}>Lancer la Visualisation</Button>
+        </a>
+      </div>
+
+      <hr />
+
+      <div className={classes.askForAddingData}>
+        <p>Votre territoire n’est pas encore disponible ?</p>
+      </div>
+      <Button onClick={() => modal.open()}>Faire une demande</Button>
+
+      <modal.Component title="Faire une demande">
+        <p>
+          Inondata fournit la visualisation de données issues de Plans de Préventions Risque
+          Inondation ou des données Territoires Risques Inondation. Si vous souhaitez visualiser les
+          données de votre territoire, veuillez renseigner votre adresse mail pour que nous
+          revenions vers vous.
+        </p>
+        <a href="mailto:inondata@ign.fr">
+          <Button>Faire une demande</Button>
+        </a>
+      </modal.Component>
     </div>
   );
 };
