1d9c943cfeb94768ce2c5eb5dac7bb9ca8d8a90b
Desplandis <75338405+Desplandis@users.noreply.github.com>
2023-06-16 10:18:18 +0200

    Support of DDT45 3D datasets (#42)
    * Add ddt45 3D data and rework ddt67 3D data
    
    * Support of new DDT67 data
    
    * Factorize 3D sources

--- src/utils/water3DLayers.tsx
+++ src/utils/water3DLayers.tsx
@@ -1,50 +1,79 @@
-import { WMTSSource, Fetcher } from "itowns";
+import { WMTSSource, Fetcher, Source } from "itowns";
 import * as THREE from "three";
 import { AggregateSource, WaterLayer } from "inondata-itowns";
 
-export type AvailableWaterLayer = "WATER";
+export type AvailableWaterLayer = "DDT67" | "DDT45";
 
 export const WaterLayerToLabel: { [layer in AvailableWaterLayer]: string } = {
-  WATER: "Inondation 3D",
+  DDT67: "Inondation 3D",
+  DDT45: "Inondation 3D (Faible probabilité)",
 };
 
-// TODO: Change unsound WMTSSource type in itowns.
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const waterDisplacementSource: any = new WMTSSource({
-  url: "https://wxs.ign.fr/altimetrie/geoportail/wmts",
-  crs: "EPSG:4326",
-  format: "image/x-bil;bits=32",
-  name: "ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES",
-  tileMatrixSet: "WGS84G",
-  zoom: { min: 11, max: 14 },
-  parser: (height: THREE.DataTexture) => Promise.resolve(height),
-});
+function defaultDisplacementSource(): Source {
+  // TODO: Change unsound WMTSSource type in itowns.
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const ignDisplacementSource: any = new WMTSSource({
+    url: "https://wxs.ign.fr/altimetrie/geoportail/wmts",
+    crs: "EPSG:4326",
+    format: "image/x-bil;bits=32",
+    name: "ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES",
+    tileMatrixSet: "WGS84G",
+    zoom: { min: 11, max: 14 },
+    parser: (height: THREE.DataTexture) => Promise.resolve(height),
+  });
 
-// TODO: Change unsound WMTSSource type in itowns.
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const waterHeigtMapSource: any = new WMTSSource({
-  url: "https://geoserver.bogato.fr/geoserver/test/gwc/service/wmts",
-  crs: "EPSG:4326",
-  format: "image/png",
-  name: "test:EMS_q100_hauteur_raster",
-  style: "test:raster_greyscale",
-  tileMatrixSet: "EPSG:4326",
-  tileMatrixCallback: (level: number) => `EPSG:4326:${level}`,
-  fetcher: Fetcher.texture,
-  parser: (height: THREE.DataTexture) => {
-    return Promise.resolve(height);
-  },
-});
+  return ignDisplacementSource;
+}
 
-// TODO: Create a Source interface or inherit Source class.
+function greyscaleHeightSource(name: string, style: string): Source {
+  // TODO: Change unsound WMTSSource type in itowns.
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const heightMapSource: any = new WMTSSource({
+    url: "https://geoserver.bogato.fr/geoserver/inondata/gwc/service/wmts",
+    crs: "EPSG:4326",
+    format: "image/png",
+    name,
+    style,
+    tileMatrixSet: "EPSG:4326",
+    tileMatrixCallback: (level: number) => `EPSG:4326:${level}`,
+    fetcher: Fetcher.texture,
+    parser: (height: THREE.Texture) => {
+      height.flipY = true; // Implicit
+      return Promise.resolve(height);
+    },
+  });
+
+  return heightMapSource;
+}
+
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const water3DSource: any = new AggregateSource("EPSG:4326", [
-  waterDisplacementSource,
-  waterHeigtMapSource,
+const water3DSource45: any = new AggregateSource("EPSG:4326", [
+  defaultDisplacementSource(),
+  greyscaleHeightSource(
+    "inondata:DDT45_isocote_probabilite_faible",
+    "inondata:hauteur_eau_greyscale_ddt45",
+  ),
 ]);
 
-export const water3DLayer = new WaterLayer(WaterLayerToLabel.WATER, {
-  source: water3DSource,
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+const water3DSource67: any = new AggregateSource("EPSG:4326", [
+  defaultDisplacementSource(),
+  greyscaleHeightSource(
+      "inondata:strasbourg",
+      "inondata:DDT67_degrade_gris",
+  ),
+]);
+
+export const water3DLayer45 = new WaterLayer(WaterLayerToLabel.DDT45, {
+  source: water3DSource45,
   zoom: { min: 11, max: 19 },
+  heightBias: 0,
   heightScale: 10,
+});
+
+export const water3DLayer67 = new WaterLayer(WaterLayerToLabel.DDT67, {
+  source: water3DSource67,
+  zoom: { min: 11, max: 19 },
+  heightBias: 0,
+  heightScale: 1,
 });
